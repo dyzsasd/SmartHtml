5ebf229ccbddf22e694d6daf6581d3782d4c210c
cjq <1401875340@qq.com>
2023-12-13 22:15:40 +0800

    update dockerfile

--- smart_html_client/src/request/request.ts
+++ smart_html_client/src/request/request.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosRequestConfig, AxiosResponse, AxiosError } from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://127.0.0.1:5000/api',
+  baseURL: '/api',
   headers: {'Content-Type': 'application/json;charset=utf-8'}
 });
 console.log(import.meta)
